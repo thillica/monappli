c6da09e8c0dd460bc0ff7489bcfb77bd40675d07
carine thilliez-pietu <carine.thilliez@gmail.com>
2021-11-26 08:59:11 +0100

    nouveau bat pour jenkins

---
+++ monappli_byJenkins.bat
@@ -0,0 +1,24 @@
+@echo on
+
+SET mypath=%~dp0
+SET tomcatpath=C:\Program Files\Apache Software Foundation\Tomcat 9.0
+
+call mvn scm:checkout -DconnectionUrl=scm:git:git@github.com:thillica/monappli.git -DcheckoutDirectory=Livraison
+
+cd %mypath%\Livraison\
+
+call mvn clean package
+
+SET src=%mypath%\Livraison\monappli-web\target\monappli-web.war
+SET dest="C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps"
+
+copy %src% %dest%
+
+cd %tomcatpath%\bin
+
+call startup.bat
+
+start chrome http://localhost:8081/monappli-web/
+pause
+
+
